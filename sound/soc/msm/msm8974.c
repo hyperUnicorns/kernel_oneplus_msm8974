57089c2ac2b4f24d9626436abb6cb1416309de91
Dorimanx <yuri@bynet.co.il>
2016-10-11 21:33:08 -0300

    sound: soc: timeout ADSP for 3s before bringing taiko up
    Just for the sake of paring the branches.
    
    Change-Id: I0f0409a48145e4977cacd6290507d3833b346399
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- sound/soc/msm/msm8974.c
+++ sound/soc/msm/msm8974.c
@@ -82,7 +82,7 @@ static int msm8974_auxpcm_rate = 8000;
 
 static void *adsp_state_notifier;
 
-#define ADSP_STATE_READY_TIMEOUT_MS 50
+#define ADSP_STATE_READY_TIMEOUT_MS 3000
 
 static inline int param_is_mask(int p)
 {
